8d6e55bc40c275454738d6b72a20a10523d9827d
Gaffen <tonedeafmessiah@gmail.com>
2012-10-20 10:22:12 +0100

    jQuery removed. New code might need some refactoring

--- frontend/js/ticker-1.2.js
+++ frontend/js/ticker-1.2.js
@@ -1,31 +1,137 @@
-var itemtime = 5000;
+var itemtime = 6000;
+var TimeToFade = 500.0;
+var msgIteration = 0;
 
 var currver = null;
 
+
 function getTickerRSS(){
-	jQuery.ajax({
-		url: "../../sukey/api/sukey-1.2.php",
-		success: function(data){
-			var sukey = jQuery(data).children('sukey');
-			var ver = sukey.attr('version');
-			if ((currver !== null) && (ver != currver))
-				window.location.reload(true);
-			currver = ver;
-			var anns = sukey.find('tickertext').contents();
-			ticker(anns,0);
-		}
-	});
+  
+  /*=======================XML Ajax Request=======================
+ *    Parse json in javascript from suke public absolute path
+ *    Last edit: 10/20/2012 by Matt Gaffen
+ *=======================XML Ajax Request=======================*/
+  
+  var xmlhttp;
+  if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  }
+  else{// code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  
+  //What to do once recieving ajax response:
+  
+  xmlhttp.onreadystatechange=function(){
+  if (xmlhttp.readyState==4 && xmlhttp.status==200){
+      feed = xmlhttp.responseXML;
+      ticker(feed, 0);
+    }
+  }
+  
+  
+  /*As we're fetching public data use 'GET' rather than 'POST'
+   *http://www.w3schools.com/ajax/ajax_xmlhttprequest_send.asp*/
+  
+  xmlhttp.open("GET","http://www.sukey.org/sukey/api/sukey-1.2.php",true);
+  xmlhttp.send();
+  
 }
 
-function ticker(list,num){
-	if(num >= list.length){
-		setTimeout(getTickerRSS,itemtime);
-	}else{
-		jQuery("#ticker").animate(
-			{ opacity: 0 }, 500,
-			function() { jQuery(this).text(jQuery(list[num]).text()) })
-		.animate(
-			{ opacity: 1 }, 500,
-			function() { setTimeout(function() { ticker(list, num+1) }, itemtime) });
-	}
+
+/*=======================Fade Animations=======================
+ *    To replace jQuery Functionality. Could probably do with refactoring.
+ *    Last edit: 10/20/2012 by Matt Gaffen
+ *=======================Fade Animations=======================*/
+
+//Fade timer function - can be re-used across the web app
+
+function fade(eid){
+  var element = document.getElementById('ticker');
+  if(element.FadeState == null)
+  {
+    if(element.style.opacity == null
+        || element.style.opacity == ''
+        || element.style.opacity == '1')
+    {
+      element.FadeState = 2;
+    }
+    else
+    {
+      element.FadeState = -2;
+    }
+  }
+   
+  if(element.FadeState == 1 || element.FadeState == -1)
+  {
+    element.FadeState = element.FadeState == 1 ? -1 : 1;
+    element.FadeTimeLeft = TimeToFade - element.FadeTimeLeft;
+  }
+  else
+  {
+    element.FadeState = element.FadeState == 2 ? -1 : 1;
+    element.FadeTimeLeft = TimeToFade;
+    setTimeout("animateFade(" + new Date().getTime() + ",'" + eid + "')", 33);
+  }  
+}
+
+//Fade animation function - can be re-used across the web app
+
+function animateFade(lastTick, eid)
+{  
+  var curTick = new Date().getTime();
+  var elapsedTicks = curTick - lastTick;
+ 
+  var element = document.getElementById(eid);
+ 
+  if(element.FadeTimeLeft <= elapsedTicks)
+  {
+    element.style.opacity = element.FadeState == 1 ? '1' : '0';
+    element.style.filter = 'alpha(opacity = '
+        + (element.FadeState == 1 ? '100' : '0') + ')';
+    element.FadeState = element.FadeState == 1 ? 2 : -2;
+    return;
+  }
+ 
+  element.FadeTimeLeft -= elapsedTicks;
+  var newOpVal = element.FadeTimeLeft/TimeToFade;
+  if(element.FadeState == 1)
+    newOpVal = 1 - newOpVal;
+
+  element.style.opacity = newOpVal;
+  element.style.filter = 'alpha(opacity = ' + (newOpVal*100) + ')';
+ 
+  setTimeout("animateFade(" + curTick + ",'" + eid + "')", 33);
+}
+
+
+//Iterate Through Updates
+
+function switchStatus(list, num){
+  fade('ticker');
+  setTimeout(function(){document.getElementById("ticker").innerHTML=
+  list.getElementsByTagName("tickertext")[msgIteration].childNodes[0].nodeValue}, TimeToFade);
+  setTimeout(function(){fade('ticker')}, TimeToFade);
+  msgIteration++
+  alert(list.getElementsByTagName("tickertext").length);
+}
+
+
+/*=======================Initiate Ticker=======================
+ *    Begin iterating through updates
+ *    Last edit: 10/20/2012 by Matt Gaffen
+ *=======================Initiate Ticker=======================*/
+
+function ticker(list){
+	setInterval(function(){
+    fade('ticker');
+    setTimeout(function(){document.getElementById("ticker").innerHTML=
+    list.getElementsByTagName("tickertext")[msgIteration].childNodes[0].nodeValue}, TimeToFade);
+    setTimeout(function(){fade('ticker')}, TimeToFade);
+    if(msgIteration >= list.getElementsByTagName("tickertext").length - 1){
+      msgIteration = 0
+    } else {
+      msgIteration++
+    }
+  }, itemtime);
 }
