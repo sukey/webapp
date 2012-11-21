5178949d1f50d76a13d31a76e5f8d3697d64cc1a
Samuel Carlisle <samuelcarlisle@gmail.com>
2012-11-21 09:56:55 +0000

    added stuff

---
+++ frontend/js/geojson.js
@@ -0,0 +1,132 @@
+var MarchRoute = {
+"type": "FeatureCollection",
+"features": [
+{ "type": "Feature", "properties": { "Name": "Start point", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ -0.111494, 51.510986, 0.000000 ] } }
+,
+{ "type": "Feature", "properties": { "popupContent": "March Route", "Name": "route", "Description": "" }, "geometry": { "type": "LineString", "coordinates": [ [ -0.116810, 51.510391 ], [ -0.120160, 51.509270 ], [ -0.122300, 51.506710 ], [ -0.124110, 51.500992 ], [ -0.126420, 51.501099 ], [ -0.126340, 51.504471 ], [ -0.126980, 51.506020 ], [ -0.127970, 51.507462 ], [ -0.129170, 51.507519 ], [ -0.130950, 51.507702 ], [ -0.132690, 51.507240 ], [ -0.133710, 51.508530 ], [ -0.134960, 51.509731 ], [ -0.141700, 51.507561 ], [ -0.145910, 51.505421 ], [ -0.150290, 51.503448 ], [ -0.151490, 51.503712 ], [ -0.160589, 51.508743 ] ] } }
+]
+} ;
+
+
+var LondonWifi = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+   "type": "Feature",
+   "properties": {
+    "popupContent": "Free Wifi"
+   },
+   "geometry": {
+   "type": "Point",
+   "coordinates": [-0.087, 51.520]} //this is staburcks near FS
+        },{
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Free Wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.125321, 51.508805]
+            }
+        }, {
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Free Wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.139195, 51.508304]
+            }
+  }, {
+      "type": "Feature",
+            "properties": {
+                "popupContent": "Free Wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.12532, 51.508805]
+            }
+  }, {
+      "type": "Feature",
+            "properties": {
+                "popupContent": "Free Wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.09619, 51.51461]
+            }
+  }, {
+      "type": "Feature",
+            "properties": {
+                "popupContent": "Free Wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.088094, 51.512622]
+            }
+  }, {
+      "type": "Feature",
+            "properties": {
+                "popupContent": "Free Wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.089882,51.504657]
+            }
+  }, {
+   "type": "Feature",
+            "properties": {
+                "popupContent": "Free Wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.098723,51.506692]
+            }
+  }, {
+   "type": "Feature",
+            "properties": {
+                "popupContent": "Free Wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.117878,51.502352]
+            }
+  }, {
+   "type": "Feature",
+            "properties": {
+                "popupContent": "Free Wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.113865,51.51834]
+            }
+  }, {
+   "type": "Feature",
+            "properties": {
+                "popupContent": "Free Wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.118861,51.519988]
+            }
+  }, {
+   "type": "Feature",
+            "properties": {
+                "popupContent": "Free Wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.097647,51.522516]
+            }
+  }, {
+   "type": "Feature",
+            "properties": {
+                "popupContent": "Free Wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.087879,51.526342]
+            }
+  }
+    ]
+};
